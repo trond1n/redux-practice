f5aec7a75948aef5a5c0109bd379e09856e78dcd
Yuri Trondin <lopata@2ch.hk>
2020-10-16 19:09:50 +0500

    initial commit

---
+++ src/index.js
@@ -0,0 +1,43 @@
+import { createStore } from "redux";
+const reducer = (state = 0, action) => {
+  switch (action.type) {
+    case "INC":
+      return state + 1;
+    case "DEC":
+      return state - 1;
+    case "RND":
+      return state + action.value;
+    default:
+      return state;
+  }
+};
+
+const inc = () => ({ type: "INC" });
+const dec = () => ({ type: "DEC" });
+const rnd = (value) => ({ type: "RND", value });
+const store = createStore(reducer);
+
+store.dispatch({ type: "INC" });
+store.dispatch({ type: "INC" });
+
+document.getElementById("inc").addEventListener("click", () => {
+  store.dispatch(inc());
+});
+document.getElementById("dec").addEventListener("click", () => {
+  store.dispatch(dec());
+});
+document.getElementById("rnd").addEventListener("click", () => {
+  const value = Math.floor(Math.random() * 10);
+
+  store.dispatch(rnd(value));
+});
+
+const update = () => {
+  document.getElementById("counter").textContent = store.getState();
+};
+store.subscribe(update);
+// let state = reducer(undefined, {});
+// state = reducer(state, { type: "INC" });
+// console.log(state);
+// state = reducer(state, { type: "INC" });
+// console.log(state);
